afef08f9e7052a5210b73b7dd55c0937628f0c72
redha <ahred.mbrk@gmail.com>
2025-01-10 21:57:19 +0000

    Type in JetBrainsMono

--- home-manager/fonts.nix
+++ home-manager/fonts.nix
@@ -3,7 +3,7 @@
   fonts.fontconfig.enable = true;
 
   home.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "FiraCode" "JetbrainsMono" "ProggyClean"]; })
+    (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" "ProggyClean"]; })
  ];
 
 }
