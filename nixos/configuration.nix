fa165d4399c6a253fdfd0f7b77fec8ed168c6e9e
redha <ahred.mbrk@gmail.com>
2025-01-12 15:53:13 +0000

    Oopsi didnt update version in overlay, need to automate it...

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -42,8 +42,8 @@
           src = prev.fetchFromGitHub {
             owner = "rredha";
             repo = "dwm";
-            rev = "v0.4";
-            sha256 = "sha256-ZYx/a362gZpvB6T1WlKZ2dk8MOqUbvnm/l0HaUmYxH4=";
+            rev = "v0.5";
+            sha256 = "0l1n5skkwy5ykrm5ndp7gb1vxpyhc87pc7x38dryn26sfr6jr2sv";
           };
        });
      })
