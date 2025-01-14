19207640151e5824ea74dbdbba5246504904a0fa
redha <ahred.mbrk@gmail.com>
2025-01-14 10:32:56 +0000

    Disabled TrustedHTML log warning

--- home-manager/home/web/qutebrowser/default.nix
+++ home-manager/home/web/qutebrowser/default.nix
@@ -6,13 +6,9 @@
 
   programs.qutebrowser.enable = true;
 
-  programs.qutebrowser.settings = {
-    content = {
-      javascript.log_message.excludes = {
-        "userscript:_qute_stylesheet": ["*Refused to apply inline style because it violates the following Content Security Policy directive: *"],
-        "userscript:_qute_js": ["*TrustedHTML*"]
-      };
-    };
-  };
+  programs.qutebrowser.extraConfig = ''
+c.content.javascript.log_message.excludes = {"userscript:_qute_stylesheet": ["*Refused to apply inline style because it violates the following Content Security Policy directive: *"],
+                                             "userscript:_qute_js": ["*TrustedHTML*"]}
+    '';
 
 }
